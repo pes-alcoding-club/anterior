77d5f5a05ec2a0a5c152c11888cc14eeaac2ab55
chandratop <chandratop.mail@gmail.com>
2020-07-18 19:15:54 +0530

    added profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,39 +1,17 @@
 import React from 'react';
 import Head from 'next/head';
 import '../css/Profile.module.css';
-import { Card, Typography, Avatar, Row, Col, List } from 'antd';
-import {
-	EditOutlined,
-	EllipsisOutlined,
-	SettingOutlined,
-} from '@ant-design/icons';
+import { Empty, Typography, Avatar, Space, Button } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
-const { Meta } = Card;
-const { Title, Paragraph } = Typography;
-const profile = () => {
-	const data = [
-		{
-			icon: 'fa fa-graduation-cap',
-			title: 'PES University',
-		},
-		{
-			icon: 'fa fa-map-marker',
-			title: 'Bangalore, India',
-		},
-		{
-			icon: 'fa fa-envelope',
-			title: 'abc@gmail.com',
-		},
-	];
-	const weblinks = ['xyz.com,abc.com', 'google.com'];
-	return (
-		<>
-			<Head>
+const { Title, Text, Link } = Typography; 
+
+export default () => {
+  return (
+    <>
+      <Head>
 				<meta charSet='utf-8' />
-				<meta
-					name='viewport'
-					content='initial-scale=1.0, width=device-width'
-				/>
+				<meta	name='viewport'	content='initial-scale=1.0, width=device-width' />
 				<title>User Profile</title>
 				<link
 					rel='stylesheet'
@@ -41,69 +19,60 @@ const profile = () => {
 					integrity='sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN'
 					crossOrigin='anonymous'
 				/>
-			</Head>
-			<Row>
-				<Col style={{ fontWeight: 'normal' }} span={8}>
-					<Card
-						/*hoverable={true}*/
-						style={{ width: '100%', height: '100%' }}
-						actions={[
-							<SettingOutlined key='setting' />,
-							<EditOutlined key='edit' />,
-							<EllipsisOutlined key='ellipsis' />,
-						]}
-					>
-						<div className='profile-pic'>
-							<Avatar
-								size={150}
-								alt={'profile pic'}
-								shape={'circle'}
-								src='https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'
-							/>
-							<Title level={4}>John Doe</Title>
-						</div>
-
-						<div className='card-description'>
-							<Typography>
-								<Paragraph>
-									Front-end developer, Musician, Golfer, Cook.
-								</Paragraph>
-							</Typography>
-							<div className='contact'>
-								<List
-									itemLayout='horizontal'
-									dataSource={data}
-									renderItem={(item) => (
-										<List.Item>
-											<i className={item.icon}></i>
-											{'  '}
-											{item.title}
-										</List.Item>
-									)}
-								></List>
-								<Title level={4}>Links</Title>
-								<List
-									itemLayout='horizontal'
-									dataSource={weblinks}
-									renderItem={(item) => (
-										<List.Item>
-											<a href={item}>{item}</a>
-										</List.Item>
-									)}
-								></List>
-							</div>
-						</div>
-					</Card>
-				</Col>
-				<Col
-					style={{ background: 'white', fontWeight: 'normal' }}
-					span={16}
-				>
-					col-16
-				</Col>
-			</Row>
-		</>
-	);
+      </Head>
+      <div className="profile-container">
+        <div className="profile-grid">
+          <div className="profile-details">
+            <div className="profile-details-container">
+              <div className="center-class"><Avatar size={200} icon={<UserOutlined />} /></div>
+              <div className="center-class username-div"><Title level={2}>John Doe</Title></div>
+              <div className="interests-div">
+                <Text strong style={{ fontSize: '14pt' }}>Frontend Developer, Music Producer, Public Speaker, Game Designer</Text>
+              </div>
+              <div className="contact-div">
+                <div className="contact-icon">
+                  <img src="/EDUCATION.svg" alt="Education"/>
+                </div>
+                <div className="contact-data">
+                  <Text>PES University, Ring Road Campus</Text>
+                </div>
+                <div className="contact-icon">
+                  <img src="/LOCATION.svg" alt="Location"/>
+                </div>
+                <div className="contact-data">
+                  <Text>Bangalore, India</Text>
+                </div>
+                <div className="contact-icon">
+                  <img src="/MAIL.svg" alt="Mail"/>
+                </div>
+                <div className="contact-data">
+                  <Text>example@mail.com</Text>
+                </div>
+              </div>
+              <div className="links-div">
+                <Space direction="vertical">
+                  <Title level={4}>Links</Title>
+                  <Link href="https://www.hackerearth.com/challenges/" target="_blank">
+                    Hackerearth
+                  </Link>
+                  <Link href="https://www.codechef.com/" target="_blank">
+                    Codechef
+                  </Link>
+                </Space>
+              </div>
+              <div className="edit-button center-class">
+                <Button type="primary" size='large' block>Edit Profile</Button>
+              </div>
+            </div>
+          </div>
+          <div className="profile-content">
+            <div className="profile-content-container">
+              <Title level={2}>User Ranking History</Title>
+              <div className="user-ranking-box center-class"><Empty /></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 };
-
-export default profile;
